fdff37bb44eb27fbafc67e44ce47fda224e583b5
Aditya Paswan <paswan.adi619@gmail.com>
2026-06-13 08:44:43 +0000

    Practice

---
+++ practice.sh
@@ -0,0 +1,6 @@
+hey this is adi the millioniare
+I am here learning the linux and about to post this files into the github
+The moment before i just posted the file in the github
+It means people on the internet can see and read my file.
+
+
